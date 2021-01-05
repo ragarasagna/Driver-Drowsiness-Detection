178674bca5537fcaf9c6c42111a058ba45e34b80
ragarasagna <51982703+ragarasagna@users.noreply.github.com>
2021-01-05 18:38:58 +0530

    Update ImageClassification.py

--- ImageClassification.py
+++ ImageClassification.py
@@ -1,10 +1,15 @@
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+#Tries to generate multiple data out of one isngle picture by shrinking,zooming, cropping etc
+from keras.preprocessing.image import ImageDataGenerator 
+#To model neural network
+from keras.models import Sequential    
+#Conv2D to extract features of picture, MaxPooling2D to reduce size of picture
+from keras.layers import Conv2D, MaxPooling2D     
+from keras.layers import Activation, Dropout, Flatten, Dense   
 from keras import backend as K
-import numpy as np
-from keras.preprocessing import image
+#Used to manipulate arrays
+import numpy as np   
+#Preprocessing import image to import them directly from dictionary and processes them
+from keras.preprocessing import image  
 
 
 img_width, img_height = 150, 150
@@ -16,20 +21,21 @@ nb_validation_samples = 12
 epochs = 50
 batch_size = 20
 
+#To set all images into a format(3,width,height) and checks if image in right format or not
 if K.image_data_format() == 'channels_first' :
     input_shape = (3, img_width, img_height)
 else :
     input_shape = (img_width, img_height, 3)
-
+#Collects data from one image by recalling, shearing, zooming, horizontally flipping it
 train_datagen = ImageDataGenerator(
         rescale = 1. / 255,
         shear_range = 0.2,
         zoom_range = 0.2,
         horizontal_flip = True)
 
-
+#Testing data to be natural so we are rescaling it
 test_datagen = ImageDataGenerator(rescale = 1. / 255)
-
+#To get all images from directory and process it
 train_generator = train_datagen.flow_from_directory(
         train_data_dir,
         target_size = (img_width, img_height),
@@ -42,10 +48,12 @@ validation_generator = test_datagen.flow_from_directory(
         batch_size = batch_size,
         class_mode = 'binary')
 
-
+#Model is the name of neural network 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
+#Adding CNN 32 means extract 32 features from image, size of searching matrix is 3*3 pixels
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))   
+#Image should be reduces. Will have  alot of data which is not required
+model.add(Activation('relu')) 
 model.add(MaxPooling2D(pool_size = (2, 2)))
 
 model.summary()
@@ -53,22 +61,27 @@ model.summary()
 model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
+#Doing same thing but extracting 64 features from it
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
+#Flattening image from 2D to 1D
 model.add(Flatten())
-model.add(Dense(64))
+#Need hidden layers that activate with given data and then gives output 64. It takes 64 features as input
+model.add(Dense(64))  
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
-model.add(Activation('sigmoid'))
+#Output layer function is sigmoid
+model.add(Activation('sigmoid'))  
 
+#Everything done till now is displayed
 model.summary()
 
+#To compile with loss
 model.compile(loss='binary_crossentropy', optimizer = 'rmsprop', metrics = ['accuracy'])
 
+#Feeding data to model
 model.fit_generator (
         train_generator,
         steps_per_epoch = nb_train_samples // batch_size,
@@ -77,6 +90,7 @@ model.fit_generator (
         validation_steps = nb_validation_samples // batch_size)
 
 #model.save_weights('model.h5')
+#Saving model into .h5 file
 model.save('model.h5')
 
 img_pred = image.load_img('nikiopen.jpg', target_size = (150, 150))
